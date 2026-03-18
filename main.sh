eddf20c77c1d18e8139b4f44a63887737b374209
Sam Mumford <smmmfrd@gmail.com>
2026-03-18 14:17:20 -0700

    Basis of Server Running

---
+++ main.sh
@@ -0,0 +1,8 @@
+# Could just put vite build up here for the little apps, you know...
+
+python3 main.py
+# The python3 httpserver just hosts the server at the port.
+# cd public && python3 -m http.server 8888
+
+# It opens starting with index.html at whatever directory it is on.
+python3 -m http.server 8888
