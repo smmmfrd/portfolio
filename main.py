6774c5096fe17b0d51afe45285d958225f805d93
Sam Mumford <smmmfrd@gmail.com>
2026-03-18 14:29:22 -0700

    Copy from Source into the Server Directory

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
+import os
+import shutil
+
+# Where the server will serve from.
+dir_path_public = "public"
+
+def main():
+    dir_base = "./"
+    print(f"Creating {dir_path_public} directory")
+
+    if os.path.exists(dir_base + dir_path_public):
+        shutil.rmtree(dir_base + dir_path_public)
+    
+    copy_file('index.html', dir_base, dir_base + dir_path_public)
+
+
+def copy_file(filename, source_path, target_path):
+    if not os.path.exists(target_path):
+        os.mkdir(target_path)
+    
+    from_path = os.path.join(source_path, filename)
+    dest_path = os.path.join(target_path, filename)
+    print(f"* {from_path} -> {dest_path}")
+
+    if os.path.isfile(from_path):
+        shutil.copy(from_path, dest_path)
+
 if __name__ == "__main__":
-    print("Pre starting server work...")
+    main()
--- main.sh
+++ main.sh
@@ -2,7 +2,5 @@
 
 python3 main.py
 # The python3 httpserver just hosts the server at the port.
-# cd public && python3 -m http.server 8888
-
-# It opens starting with index.html at whatever directory it is on.
-python3 -m http.server 8888
+# Starts with index.html at the directory it is in.
+cd public && python3 -m http.server 8888
